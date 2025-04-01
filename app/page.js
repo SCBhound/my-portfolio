f746cbb2af16ae66bd13d4924185b69b30ae226b
dazai <tienpv@slitigenz.io>
2025-04-01 17:12:21 +0700

    finish navbar, header and responsive for both

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-'use client'
+"use client";
 
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <Header />
     </>
   );
 }
